8ada653c15818c84174a3d1aedd1995cb786fbdf
Antony Saragas <antony.saragas@gmail.com>
2025-09-25 05:50:11 -0400

    Export reference updates.

--- event_collective_backend/app/services/__init__.py
+++ event_collective_backend/app/services/__init__.py
@@ -1,11 +1,19 @@
-from .auth import (
-    authenticate_user,
-    create_access_token,
-    verify_access_token,
-)
+from . import auth as auth_service
+from . import calendar as calendar_service
+from . import contact as contact_service
+from . import gallery as gallery_service
+from . import message as message_service
+from . import review as review_service
+from . import service as service_service
+from . import user as user_service
 
-
-class _AuthServiceProxy:
-    from .auth import authenticate_user, create_access_token, verify_access_token
-
-auth_service = _AuthServiceProxy()
+__all__ = [
+    "auth_service",
+    "calendar_service",
+    "contact_service",
+    "gallery_service",
+    "message_service",
+    "review_service",
+    "service_service",
+    "user_service",
+]
